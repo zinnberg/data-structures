051cd7cecfe26c866ef4f4abac2da98e574d5f00
Michael Zinn <zinnberg@gmail.com>
2023-01-05 01:07:51 -0500

    Finished docs for bst node

--- src/trees/binary-search-tree/bst-node.js
+++ src/trees/binary-search-tree/bst-node.js
@@ -12,23 +12,41 @@ class BSTNode {
     this.left = null;
     this.right = null;
   }
-
+  /**
+   * Returns The Left Child
+   * @returns {BSTNode}
+   */
   getLeft() {
     return this.left;
   }
-
+  /**
+   * Returns The Right Child
+   * @returns {BSTNode}
+   */
   getRight() {
     return this.right;
   }
 
+  /**
+   * Checks if node has a left child
+   * @returns {boolean}
+   */
   hasLeft() {
     return this.left != null;
   }
 
+  /**
+   * Check if node has right child
+   * @returns {boolean}
+   */
   hasRight() {
     return this.right != null;
   }
 
+  /**
+   * Checks if node is leaf
+   * @returns {boolean}
+   */
   isLeaf() {
     return !this.hasLeft() && !this.hasRight();
   }
